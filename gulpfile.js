ad693501d4bd3688331689bb09df6ab0ad48d4f4
Victor Santana Costa <39810026+victorscdev@users.noreply.github.com>
2020-02-24 06:38:38 -0300

    Base Project 100%

---
+++ gulpfile.js
@@ -0,0 +1,69 @@
+const gulp = require( 'gulp' );
+const imagemin = require( 'gulp-imagemin' );
+const minifyCss = require( 'gulp-minify-css' );
+const concat = require( 'gulp-concat' );
+const autoPrefixer = require( 'gulp-autoprefixer' );
+const browserSync = require( 'browser-sync' );
+const webserver = require( 'gulp-webserver' );
+const sass = require('gulp-sass');
+const rename = require('gulp-rename');
+
+
+
+/*
+========================
+VARIABLES
+========================
+*/
+const dirDev = "public/assets/img/**/*";
+const dirProd = "build/assets/img/";
+const dirDevCss = ['public/vendor/bootstrap/css/bootstrap.css','public/css/heroic-features.css',];
+const dirProdCss = "build/assets/css";
+
+
+
+/*
+========================
+TASKS
+========================
+*/
+
+gulp.task('browserSync', function() {
+    browserSync.init({
+        server: {
+            baseDir: "build"
+        }
+    });
+});
+
+gulp.task('img-min', function() {
+    return gulp.src(dirDev)
+        .pipe(imagemin())
+        .pipe(gulp.dest(dirProd));
+});
+
+gulp.task('css-min', function() {
+    return gulp.src(dirDevCss)
+        .pipe(concat('main.bootstrap.min.css'))
+        .pipe(autoPrefixer('last 1 version'))
+        .pipe(minifyCss())
+        .pipe(gulp.dest(dirProdCss))
+        .pipe(browserSync.reload({
+            stream: true
+        }));
+});
+
+gulp.task('webserver', function() {
+    gulp.src('app')
+      .pipe(webserver({
+        livereload: true,
+        directoryListing: true,
+        open: true
+      }));
+  });
+
+gulp.task('watch', function() {
+    return gulp.watch(dirDevCss, gulp.series('css-min'));
+});
+
+gulp.task('default', gulp.parallel('browserSync', 'img-min', 'css-min', 'webserver', 'watch'), function() { });
